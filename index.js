7fe7da2fdf1ce13e28256facb14e708a17f8a41b
2005869 <2005869@aluno.univesp.br>
2023-01-21 15:55:09 -0300

    connect back with front

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const PORT = 8080;
 const express = require('express');
+const { Socket } = require('socket.io');
 var app = express();
 
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+    console.log(socket);
+    console.log(socket.id);
+});
 
 app.set('view engine', 'ejs');
 
